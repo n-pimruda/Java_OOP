4e93bfd49a2123553422721757662cd806018eb1
n-pimruda <161825930+n-pimruda@users.noreply.github.com>
2024-03-02 14:15:09 +0700

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -2,7 +2,22 @@ package week6;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student = new Student("chanyut", "lisawat", 70, 74, 99);
-        System.out.println("The average score of chanyut is: " + student.getAverage());
+
+        Student student = new Student("Pimruda", "Chumsuk", 80, 90, 85);
+ 
+        System.out.println("name: " + student.getName());
+        System.out.println("lastname: " + student.getSurname());
+        System.out.println("AverageScore: " + student.getAverage());
+ 
+        student.setName("Pimruda");
+        student.setSurname("Chumsuk");
+        student.setScore1(90);
+        student.setScore2(95);
+        student.setScore3(100);
+ 
+        System.out.println("\n name: " + student.getName());
+        System.out.println("lastname: " + student.getSurname());
+        System.out.println("AverageScore: " + student.getAverage());
+ 
     }
 }
