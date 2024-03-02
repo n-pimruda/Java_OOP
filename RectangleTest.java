267d0ffded78e723aa8c46adcf23e0e4bdb8ddfd
n-pimruda <161825930+n-pimruda@users.noreply.github.com>
2024-03-02 14:03:22 +0700

    Update RectangleTest.java

--- RectangleTest.java
+++ RectangleTest.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class RectangleTest {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+        
     int width = scanner.nextInt();
     int height = scanner.nextInt();
     Rectangle Rectangle1 = new Rectangle(width, height);
+        
     int area = Rectangle1.area();
     int perimeter = Rectangle1.perimeter();
+        
     System.out.printf("area: %d",area);
     System.out.println();
     System.out.printf("perimeter: %d",perimeter);
