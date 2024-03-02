6c35e3a7b218317a436642800fa65855e1b677ad
n-pimruda <161825930+n-pimruda@users.noreply.github.com>
2024-03-02 14:22:18 +0700

    Create GradeBook.java

---
+++ GradeBook.java
@@ -0,0 +1,32 @@
+//Lab 2: GradeBook.java 
+//GradeBook class with a constructor to initialize the course name. 
+public class GradeBook 
+{
+  private String courseName;
+  private String instructorName;
+  public GradeBook( String name )  {   
+    courseName = name; 
+  } 
+  
+  public void setCourseName( String name ) { 
+    courseName = name; 
+  } 
+  
+  public String getCourseName() { 
+    return courseName;
+  } 
+  
+  public void setInstructorName(String name) {
+    instructorName = name;
+  }
+  
+  public String getInstructorName() {
+    return instructorName;
+	}
+  
+	public void displayMessage() {
+		System.out.printf( "Welcome to the grade book for\n%s!\n", getCourseName() );
+		System.out.printf("This course is presented by: %s\n", getInstructorName());		
+	} 
+} 
+
